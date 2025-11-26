93be195b57ce42c9e1646caa224efe2e748f6834
francisatanazie-netizen <francisatanazie@gmail.com>
2025-11-25 16:44:11 -0800

    Create main.tsx

---
+++ linkoova---next-gen-agency/main.tsx
@@ -0,0 +1,10 @@
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import './i18n.ts'; // <--- ASSUREZ-VOUS QUE C'EST PRÉSENT
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // ...
+);
