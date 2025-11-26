99f1e05b0802d60d5d3f6f655c209e3133959105
francisatanazie-netizen <francisatanazie@gmail.com>
2025-11-25 17:23:54 -0800

    Update main.tsx

--- linkoova---next-gen-agency/main.tsx
+++ linkoova---next-gen-agency/main.tsx
@@ -1,10 +1,19 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import './i18n.ts'; // <--- ASSUREZ-VOUS QUE C'EST PRÉSENT
+
+// 1. Import de la configuration du moteur de traduction
+import i18n from './i18n.ts'; 
+
+// 2. Import du composant Fournisseur de Contexte React-i18next
+import { I18nextProvider } from 'react-i18next'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // ...
+  <React.StrictMode>
+    {/* 3. ENVELOPPEZ l'application ici pour que useTranslation fonctionne partout */}
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
 );
