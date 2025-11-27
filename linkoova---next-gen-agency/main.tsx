6f062924b923bf1cb4eacfa2c9eb46742d7e47ba
francisatanazie-netizen <francisatanazie@gmail.com>
2025-11-27 01:39:06 -0800

    Update main.tsx

--- linkoova---next-gen-agency/main.tsx
+++ linkoova---next-gen-agency/main.tsx
@@ -4,16 +4,22 @@ import App from './App.tsx';
 import './index.css';
 
 // 1. Import de la configuration du moteur de traduction
-import i18n from './i18n.ts'; 
+import i18n from './i18n.ts'; 
 
 // 2. Import du composant Fournisseur de Contexte React-i18next
-import { I18nextProvider } from 'react-i18next'; 
+import { I18nextProvider } from 'react-i18next'; 
+
+// 🛑 NOUVEL IMPORT : Le fournisseur de routage
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {/* 3. ENVELOPPEZ l'application ici pour que useTranslation fonctionne partout */}
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </React.StrictMode>,
+  <React.StrictMode>
+    {/* 🛑 ÉTAPE 2 : BrowserRouter doit être le parent pour le routage de l'URL */}
+    <BrowserRouter>
+      {/* I18nextProvider est maintenant à l'intérieur du routeur */}
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
